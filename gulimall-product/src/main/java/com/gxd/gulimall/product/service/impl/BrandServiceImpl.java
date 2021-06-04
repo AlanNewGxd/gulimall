8ba0fd726ada2c0af0e6601a418b5df4173132fd
AlanNewGxd <86466280@qq.com>
2021-06-04 17:09:28 +0800

    新增商品服务-分页配置插件以及品牌列表模糊查询功能

--- gulimall-product/src/main/java/com/gxd/gulimall/product/service/impl/BrandServiceImpl.java
+++ gulimall-product/src/main/java/com/gxd/gulimall/product/service/impl/BrandServiceImpl.java
@@ -1,23 +1,23 @@
 package com.gxd.gulimall.product.service.impl;
 
-import org.springframework.stereotype.Service;
-import java.util.Map;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.gxd.common.utils.PageUtils;
 import com.gxd.common.utils.Query;
-
 import com.gxd.gulimall.product.dao.BrandDao;
 import com.gxd.gulimall.product.entity.BrandEntity;
 import com.gxd.gulimall.product.service.BrandService;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
 
 
 @Service("brandService")
 public class BrandServiceImpl extends ServiceImpl<BrandDao, BrandEntity> implements BrandService {
 
-    @Override
-    public PageUtils queryPage(Map<String, Object> params) {
+    public PageUtils queryPage2(Map<String, Object> params) {
         IPage<BrandEntity> page = this.page(
                 new Query<BrandEntity>().getPage(params),
                 new QueryWrapper<BrandEntity>()
@@ -26,4 +26,26 @@ public class BrandServiceImpl extends ServiceImpl<BrandDao, BrandEntity> impleme
         return new PageUtils(page);
     }
 
+    /**
+     * @Description 改造模糊查询wrapper
+     * @Param [params]
+     * @Author guxiaodong
+     * @Date 15:44 2021/6/4
+     **/
+    @Override
+    public PageUtils queryPage(Map<String, Object> params) {
+        QueryWrapper<BrandEntity> wrapper = new QueryWrapper<>();
+        String key = params.get("key").toString();
+        if(!StringUtils.isEmpty(key)){
+            // 字段等于  or  模糊查询
+            wrapper.eq("brand_id", key).or().like("name", key);
+        }
+        // 按照分页信息和查询条件  进行查询
+        IPage<BrandEntity> page = this.page(
+                // 传入一个IPage对象，他是接口，实现类是Page
+                new Query<BrandEntity>().getPage(params),
+                wrapper
+        );
+        return new PageUtils(page);
+    }
 }
