e33eaaa8bc3deb692f30a0ce0ef5415be478f0fc
AlanNewGxd <86466280@qq.com>
2021-06-22 16:45:33 +0800

    新增仓储服务-API-仓库管理-整合ware服务&实现仓库维护功能

--- gulimall-ware/src/main/java/com/gxd/gulimall/ware/controller/WareInfoController.java
+++ gulimall-ware/src/main/java/com/gxd/gulimall/ware/controller/WareInfoController.java
@@ -1,19 +1,14 @@
 package com.gxd.gulimall.ware.controller;
 
-import java.util.Arrays;
-import java.util.Map;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.gxd.gulimall.ware.entity.WareInfoEntity;
-import com.gxd.gulimall.ware.service.WareInfoService;
 import com.gxd.common.utils.PageUtils;
 import com.gxd.common.utils.R;
+import com.gxd.gulimall.ware.entity.WareInfoEntity;
+import com.gxd.gulimall.ware.service.WareInfoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
+import java.util.Map;
 
 
 
@@ -32,6 +27,7 @@ public class WareInfoController {
 
     /**
      * 列表
+     * 仓库维护-检索
      */
     @RequestMapping("/list")
     public R list(@RequestParam Map<String, Object> params){
--- gulimall-ware/src/main/java/com/gxd/gulimall/ware/service/impl/WareInfoServiceImpl.java
+++ gulimall-ware/src/main/java/com/gxd/gulimall/ware/service/impl/WareInfoServiceImpl.java
@@ -1,26 +1,46 @@
 package com.gxd.gulimall.ware.service.impl;
 
-import org.springframework.stereotype.Service;
-import java.util.Map;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.gxd.common.utils.PageUtils;
 import com.gxd.common.utils.Query;
-
 import com.gxd.gulimall.ware.dao.WareInfoDao;
 import com.gxd.gulimall.ware.entity.WareInfoEntity;
 import com.gxd.gulimall.ware.service.WareInfoService;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
 
 
 @Service("wareInfoService")
 public class WareInfoServiceImpl extends ServiceImpl<WareInfoDao, WareInfoEntity> implements WareInfoService {
 
+//    @Override
+//    public PageUtils queryPage(Map<String, Object> params) {
+//        IPage<WareInfoEntity> page = this.page(
+//                new Query<WareInfoEntity>().getPage(params),
+//                new QueryWrapper<WareInfoEntity>()
+//        );
+//
+//        return new PageUtils(page);
+//    }
+
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
+        QueryWrapper<WareInfoEntity> queryWrapper = new QueryWrapper<>();
+        String key = (String)params.get("key");
+        if (!StringUtils.isEmpty(key)) {
+            queryWrapper.eq("id", key).or()
+                    .like("name", key).or()
+                    .like("address", key).or()
+                    .like("areacode", key);
+        }
+
         IPage<WareInfoEntity> page = this.page(
                 new Query<WareInfoEntity>().getPage(params),
-                new QueryWrapper<WareInfoEntity>()
+                queryWrapper
         );
 
         return new PageUtils(page);
