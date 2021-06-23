522d17e2b5f2a67829ed68e031436e3ec827d4a2
AlanNewGxd <86466280@qq.com>
2021-06-23 15:30:38 +0800

    新增仓储服务-API-仓库管理-实现商品库存工作单查询功能；新增，修改有bug

--- gulimall-ware/src/main/java/com/gxd/gulimall/ware/service/impl/WareOrderTaskServiceImpl.java
+++ gulimall-ware/src/main/java/com/gxd/gulimall/ware/service/impl/WareOrderTaskServiceImpl.java
@@ -1,26 +1,46 @@
 package com.gxd.gulimall.ware.service.impl;
 
-import org.springframework.stereotype.Service;
-import java.util.Map;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.gxd.common.utils.PageUtils;
 import com.gxd.common.utils.Query;
-
 import com.gxd.gulimall.ware.dao.WareOrderTaskDao;
 import com.gxd.gulimall.ware.entity.WareOrderTaskEntity;
 import com.gxd.gulimall.ware.service.WareOrderTaskService;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
 
 
 @Service("wareOrderTaskService")
 public class WareOrderTaskServiceImpl extends ServiceImpl<WareOrderTaskDao, WareOrderTaskEntity> implements WareOrderTaskService {
 
+//    @Override
+//    public PageUtils queryPage(Map<String, Object> params) {
+//        IPage<WareOrderTaskEntity> page = this.page(
+//                new Query<WareOrderTaskEntity>().getPage(params),
+//                new QueryWrapper<WareOrderTaskEntity>()
+//        );
+//
+//        return new PageUtils(page);
+//    }
+
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
+        QueryWrapper<WareOrderTaskEntity> queryWrapper = new QueryWrapper<>();
+        String key = (String)params.get("key");
+        if (!StringUtils.isEmpty(key)) {
+            queryWrapper.eq("id", key).or()
+                    .like("consignee", key).or()
+                    .like("consignee_tel", key).or()
+                    .like("delivery_address", key);
+        }
+
         IPage<WareOrderTaskEntity> page = this.page(
                 new Query<WareOrderTaskEntity>().getPage(params),
-                new QueryWrapper<WareOrderTaskEntity>()
+                queryWrapper
         );
 
         return new PageUtils(page);
