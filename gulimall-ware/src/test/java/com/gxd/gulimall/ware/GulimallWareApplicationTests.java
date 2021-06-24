3aa37c8544f374330703d84da4ab2065717abcf3
AlanNewGxd <86466280@qq.com>
2021-06-24 15:24:24 +0800

    新增仓储服务-API-仓库管理-实现商品采购单维护功能2

--- gulimall-ware/src/test/java/com/gxd/gulimall/ware/GulimallWareApplicationTests.java
+++ gulimall-ware/src/test/java/com/gxd/gulimall/ware/GulimallWareApplicationTests.java
@@ -3,11 +3,22 @@ package com.gxd.gulimall.ware;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 @SpringBootTest
 class GulimallWareApplicationTests {
 
     @Test
     void contextLoads() {
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4);
+
+        String commaSeparatedNumbers = numbers.stream()
+                .map(i -> i.toString())
+                .collect(Collectors.joining(", "));
+
+        System.out.println(commaSeparatedNumbers);
     }
 
 }
