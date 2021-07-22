da7d98ccf8e17f78ae79409f86e480e7d2060f62
AlanNewGxd <86466280@qq.com>
2021-07-22 11:20:08 +0800

    新增商城业务-异步-CompletableFuture-handle最终处理

--- gulimall-search/src/main/java/com/gxd/gulimall/search/thread/ThreadTest2.java
+++ gulimall-search/src/main/java/com/gxd/gulimall/search/thread/ThreadTest2.java
@@ -23,20 +23,43 @@ public class ThreadTest2 {
 //             System.out.println("运行结果：" + i);
 //         }, excutor);
 
-        CompletableFuture<Integer> future1 = CompletableFuture.supplyAsync(() -> {
-            System.out.println("当前线程：" + Thread.currentThread().getId());
-            int i = 10 / 0;
-            System.out.println("运行结果：" + i);
-            return i;
-        }, excutor).whenComplete((result,exception) -> {
-            //虽然能得到异常信息，但是没法修改返回数据
-            System.out.println("异步任务成功完成了...结果是：" + result + "异常是：" + exception);
-        }).exceptionally(throwable -> {
-            //可以感知异常，同时返回默认值
-            return 10;
-        });
+        //方法完成后的感知处理
+//        CompletableFuture<Integer> future1 = CompletableFuture.supplyAsync(() -> {
+//            System.out.println("当前线程：" + Thread.currentThread().getId());
+//            int i = 10 / 0;
+//            System.out.println("运行结果：" + i);
+//            return i;
+//        }, excutor).whenComplete((result,exception) -> {
+//            //虽然能得到异常信息，但是没法修改返回数据
+//            System.out.println("异步任务成功完成了...结果是：" + result + "异常是：" + exception);
+//        }).exceptionally(throwable -> {
+//            //可以感知异常，同时返回默认值
+//            return 10;
+//        });
+//
+//        Integer integer = future1.get();
+//        System.out.println("main......end....."+integer);
 
-        Integer integer = future1.get();
+        /**
+         * 方法执行完后端处理 handle方法
+         */
+         CompletableFuture<Integer> future2 = CompletableFuture.supplyAsync(() -> {
+             System.out.println("当前线程：" + Thread.currentThread().getId());
+             int i = 10 / 0;
+             System.out.println("运行结果：" + i);
+             return i;
+         }, excutor).handle((result,thr) -> {
+             if (result != null) {
+                 return result * 2;
+             }
+             if (thr != null) {
+                 System.out.println("异步任务成功完成了...结果是：" + result + "异常是：" + thr);
+                 return 0;
+             }
+             return 0;
+         });
+
+        Integer integer = future2.get();
         System.out.println("main......end....."+integer);
     }
 }
