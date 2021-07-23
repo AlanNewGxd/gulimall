cf4b823a9322fef0964a8b6f88970206179f9d62
AlanNewGxd <86466280@qq.com>
2021-07-23 16:38:48 +0800

    新增商城业务-商品检索-gulimall-search更换es服务地址

--- gulimall-search/src/main/java/com/gxd/gulimall/search/config/GuliESConfig.java
+++ gulimall-search/src/main/java/com/gxd/gulimall/search/config/GuliESConfig.java
@@ -30,7 +30,7 @@ public class GuliESConfig {
 
         RestClientBuilder builder = null;
         // 可以指定多个es
-        builder = RestClient.builder(new HttpHost("121.89.210.14", 9200, "http"));
+        builder = RestClient.builder(new HttpHost("192.168.56.10", 9200, "http"));
 
         RestHighLevelClient client = new RestHighLevelClient(builder);
         return client;
