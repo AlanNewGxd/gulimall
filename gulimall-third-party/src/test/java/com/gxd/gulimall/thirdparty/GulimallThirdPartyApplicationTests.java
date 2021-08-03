842675298808dce6a2a5b9931324dbe7241bfd08
AlanNewGxd <86466280@qq.com>
2021-08-03 15:26:16 +0800

    新增商城业务-认证服务-实现自定义验证码组件

--- gulimall-third-party/src/test/java/com/gxd/gulimall/thirdparty/GulimallThirdPartyApplicationTests.java
+++ gulimall-third-party/src/test/java/com/gxd/gulimall/thirdparty/GulimallThirdPartyApplicationTests.java
@@ -1,24 +1,25 @@
 package com.gxd.gulimall.thirdparty;
 
 import com.aliyun.oss.OSSClient;
-
-import com.gxd.common.utils.HttpUtils;
-import org.apache.http.HttpResponse;
+import com.gxd.gulimall.thirdparty.component.SmsComponent;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.io.FileInputStream;
+import javax.annotation.Resource;
 import java.io.FileNotFoundException;
-import java.io.InputStream;
-import java.util.HashMap;
-import java.util.Map;
 
 @SpringBootTest
 class GulimallThirdPartyApplicationTests {
 
-    @Autowired
+    @Resource
     OSSClient ossClient;
+    @Resource
+    SmsComponent smsComponent;
+
+    @Test
+    void testSms() {
+        smsComponent.sendCode("16602789607", "996655");
+    }
 
     @Test
     void contextLoads() {
@@ -36,48 +37,48 @@ class GulimallThirdPartyApplicationTests {
 //        OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
 
         //上传文件流。
-        InputStream inputStream = new FileInputStream("C:\\Users\\AlanNew\\Pictures\\test\\ceshi.jpg");
-        ossClient.putObject("gulimall-alannew", "333.jpg", inputStream);
-
-        // 关闭OSSClient。
-        ossClient.shutdown();
-        System.out.println("上传成功.");
+//        InputStream inputStream = new FileInputStream("C:\\Users\\AlanNew\\Pictures\\test\\ceshi.jpg");
+//        ossClient.putObject("gulimall-alannew", "333.jpg", inputStream);
+//
+//        // 关闭OSSClient。
+//        ossClient.shutdown();
+//        System.out.println("上传成功.");
     }
 
     @Test
     public void sendsms(){
 
-        String host = "https://gyytz.market.alicloudapi.com";
-        String path = "/sms/smsSend";
-        String method = "POST";
-        String appcode = "appcode";
-        Map<String, String> headers = new HashMap<String, String>();
-        //最后在header中的格式(中间是英文空格)为Authorization:APPCODE 83359fd73fe94948385f570e3c139105
-        headers.put("Authorization", "APPCODE " + appcode);
-        Map<String, String> querys = new HashMap<String, String>();
-        querys.put("mobile", "18001130501");
-        querys.put("param", "**code**:12345,**minute**:5");
-        querys.put("smsSignId", "2e65b1bb3d054466b82f0c9d125465e2");
-        querys.put("templateId", "908e94ccf08b4476ba6c876d13f084ad");
-        Map<String, String> bodys = new HashMap<String, String>();
-
-        try {
-            /**
-             * 重要提示如下:
-             * HttpUtils请从
-             * https://github.com/aliyun/api-gateway-demo-sign-java/blob/master/src/main/java/com/aliyun/api/gateway/demo/util/HttpUtils.java
-             * 下载
-             *
-             * 相应的依赖请参照
-             * https://github.com/aliyun/api-gateway-demo-sign-java/blob/master/pom.xml
-             */
-            HttpResponse response = HttpUtils.doPost(host, path, method, headers, querys, bodys);
-            System.out.println(response.toString());
-            //获取response的body
-            //System.out.println(EntityUtils.toString(response.getEntity()));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+//        String host = "https://gyytz.market.alicloudapi.com";
+//        String path = "/sms/smsSend";
+//        String method = "POST";
+//        String appcode = "appcode";
+//        Map<String, String> headers = new HashMap<String, String>();
+//        //最后在header中的格式(中间是英文空格)为Authorization:APPCODE 83359fd73fe94948385f570e3c139105
+//        headers.put("Authorization", "APPCODE " + appcode);
+//        Map<String, String> querys = new HashMap<String, String>();
+//        querys.put("mobile", "18001130501");
+//        querys.put("param", "**code**:12345,**minute**:5");
+//        querys.put("smsSignId", "2e65b1bb3d054466b82f0c9d125465e2");
+//        querys.put("templateId", "908e94ccf08b4476ba6c876d13f084ad");
+//        Map<String, String> bodys = new HashMap<String, String>();
+//
+//        try {
+//            /**
+//             * 重要提示如下:
+//             * HttpUtils请从
+//             * https://github.com/aliyun/api-gateway-demo-sign-java/blob/master/src/main/java/com/aliyun/api/gateway/demo/util/HttpUtils.java
+//             * 下载
+//             *
+//             * 相应的依赖请参照
+//             * https://github.com/aliyun/api-gateway-demo-sign-java/blob/master/pom.xml
+//             */
+//            HttpResponse response = HttpUtils.doPost(host, path, method, headers, querys, bodys);
+//            System.out.println(response.toString());
+//            //获取response的body
+//            //System.out.println(EntityUtils.toString(response.getEntity()));
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
     }
 
 }
