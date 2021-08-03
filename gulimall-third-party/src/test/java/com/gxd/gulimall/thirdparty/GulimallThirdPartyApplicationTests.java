922f5f32ba9f4d42c3ac99f98593270ab0d4efa0
AlanNewGxd <86466280@qq.com>
2021-08-03 15:05:07 +0800

    新增商城业务-认证服务-单元测试短信验证码

--- gulimall-third-party/src/test/java/com/gxd/gulimall/thirdparty/GulimallThirdPartyApplicationTests.java
+++ gulimall-third-party/src/test/java/com/gxd/gulimall/thirdparty/GulimallThirdPartyApplicationTests.java
@@ -1,6 +1,9 @@
 package com.gxd.gulimall.thirdparty;
 
 import com.aliyun.oss.OSSClient;
+
+import com.gxd.common.utils.HttpUtils;
+import org.apache.http.HttpResponse;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -8,6 +11,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootTest
 class GulimallThirdPartyApplicationTests {
@@ -24,8 +29,8 @@ class GulimallThirdPartyApplicationTests {
         // Endpoint以杭州为例，其它Region请按实际情况填写。
 //        String endpoint = "oss-cn-zhangjiakou.aliyuncs.com";
         // 云账号AccessKey有所有API访问权限，建议遵循阿里云安全最佳实践，创建并使用RAM子账号进行API访问或日常运维，请登录 https://ram.console.aliyun.com 创建。
-//        String accessKeyId = "LTAI5tJiMCkdzm88Q38B5uFT";
-//        String accessKeySecret = "6v6GDQQDqDJ2ATo1OdWsdtpro3zg5C";
+//        String accessKeyId = "accessKeyId";
+//        String accessKeySecret = "accessKeySecret";
 
         // 创建OSSClient实例。
 //        OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
@@ -37,6 +42,42 @@ class GulimallThirdPartyApplicationTests {
         // 关闭OSSClient。
         ossClient.shutdown();
         System.out.println("上传成功.");
+    }
+
+    @Test
+    public void sendsms(){
+
+        String host = "https://gyytz.market.alicloudapi.com";
+        String path = "/sms/smsSend";
+        String method = "POST";
+        String appcode = "appcode";
+        Map<String, String> headers = new HashMap<String, String>();
+        //最后在header中的格式(中间是英文空格)为Authorization:APPCODE 83359fd73fe94948385f570e3c139105
+        headers.put("Authorization", "APPCODE " + appcode);
+        Map<String, String> querys = new HashMap<String, String>();
+        querys.put("mobile", "18001130501");
+        querys.put("param", "**code**:12345,**minute**:5");
+        querys.put("smsSignId", "2e65b1bb3d054466b82f0c9d125465e2");
+        querys.put("templateId", "908e94ccf08b4476ba6c876d13f084ad");
+        Map<String, String> bodys = new HashMap<String, String>();
+
+        try {
+            /**
+             * 重要提示如下:
+             * HttpUtils请从
+             * https://github.com/aliyun/api-gateway-demo-sign-java/blob/master/src/main/java/com/aliyun/api/gateway/demo/util/HttpUtils.java
+             * 下载
+             *
+             * 相应的依赖请参照
+             * https://github.com/aliyun/api-gateway-demo-sign-java/blob/master/pom.xml
+             */
+            HttpResponse response = HttpUtils.doPost(host, path, method, headers, querys, bodys);
+            System.out.println(response.toString());
+            //获取response的body
+            //System.out.println(EntityUtils.toString(response.getEntity()));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
