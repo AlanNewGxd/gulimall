f6bade6f42af33b1a50800fa5e3fffe483945b03
guxiaodong <guxiaodong@chinanews.com.cn>
2022-01-13 16:10:40 +0800

    中途遗留提交分支

--- gulimall-product/src/test/java/com/gxd/gulimall/product/GulimallProductApplicationTests.java
+++ gulimall-product/src/test/java/com/gxd/gulimall/product/GulimallProductApplicationTests.java
@@ -1,35 +1,29 @@
 package com.gxd.gulimall.product;
 
 
-import com.gxd.gulimall.product.service.BrandService;
-import com.gxd.gulimall.product.service.CategoryService;
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.Test;
-import org.redisson.api.RedissonClient;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.redis.core.StringRedisTemplate;
 
 @Slf4j
 @SpringBootTest
 class GulimallProductApplicationTests {
 
-    @Autowired
-    BrandService brandService;
-
-    @Autowired
-    CategoryService categoryService;
-
-    @Autowired
-    StringRedisTemplate stringRedisTemplate;
-
-    @Autowired
-    RedissonClient redissonClient;
-
-    @Test
-    void testRedisson() {
-        System.out.println(redissonClient);
-    }
+//    @Autowired
+//    BrandService brandService;
+//
+//    @Autowired
+//    CategoryService categoryService;
+//
+//    @Autowired
+//    StringRedisTemplate stringRedisTemplate;
+//
+//    @Autowired
+//    RedissonClient redissonClient;
+//
+//    @Test
+//    void testRedisson() {
+//        System.out.println(redissonClient);
+//    }
 
 //    @Test
 //    public void testRedis(){
